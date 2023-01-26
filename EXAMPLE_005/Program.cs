f4e8253ca1a2c3f0b6472bcf894517679f4ceac1
Mutniyh <wrobotw@mail.ru>
2023-01-26 17:17:01 +0300

    задача с гирьками через while

---
+++ EXAMPLE_005/Program.cs
@@ -0,0 +1,28 @@
+﻿int [] num;
+num = new int[3];
+num[0] = 6;
+num[1] = 3;
+num[2] = 2;
+int index = 0;
+int size = 3;
+int max = num[0];
+
+while(index < size) 
+{
+    if (num[index] > max) 
+ {
+      max = num[index];
+    index = index + 1;
+   }
+   
+    else
+   {
+     index = index + 1; 
+    }   
+}
+
+
+Console.WriteLine(max);
+
+//Console.WriteLine("введите вес первой гири : ");
+//string a = Console.RiadLine();
