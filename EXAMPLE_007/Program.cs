179e19e16357d2c1bcebe2e1cf36ffe7f47d4039
Mutniyh <wrobotw@mail.ru>
2023-02-01 11:41:07 +0300

    Массив + опред длин массива + цикл

--- EXAMPLE_007/Program.cs
+++ EXAMPLE_007/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//             0  1  2  3  4  5  6  7  8  9
+int[] array = {1, 2, 5, 4, 5, 6, 7, 8, 4, 10};
+int size = array.Length;
+int find = 5;
+int index = 0;
+
+while (index < size)
+{
+if (array[index] == find) 
+{
+    Console.WriteLine($"Первое {index}");
+    break;
+}
+index ++;
+}
+index ++;
+while (index < size)
+{
+if (array[index] == find) 
+{
+    Console.WriteLine($"Второе {index}");
+    break;
+}
+index ++;
+}
+
